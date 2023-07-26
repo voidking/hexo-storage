01dda7eff280fe96bae74ca3e3851e56bb753c8d
voidking <voidking@qq.com>
2023-07-26 17:40:54 +0800

    add: host-docker-k8s-monitoring

---
+++ host-docker-k8s-monitoring/install_cadvisor.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Check if the current user is root or has sudo privileges
+if [[ $(id -u) -ne 0 ]]; then
+  echo "This script must be run as root or with sudo privileges."
+  exit 1
+fi
+
+# Function to check if Docker is installed and running
+check_docker() {
+  if command -v docker &>/dev/null && docker info &>/dev/null; then
+    return 0  # Docker is installed and running
+  else
+    return 1  # Docker is not installed or not running
+  fi
+}
+
+# Function to check if cAdvisor container is exist
+check_cadvisor() {
+  if docker ps -a --format '{{.Names}}' | grep -q "cadvisor"; then
+    return 0  # cAdvisor container is exist
+  else
+    return 1  # cAdvisor container is not exist
+  fi
+}
+
+# Function to check if cAdvisor container is running
+check_cadvisor_running() {
+  if docker ps --format '{{.Names}}' | grep -q "cadvisor"; then
+    return 0  # cAdvisor container is running
+  else
+    return 1  # cAdvisor container is not running
+  fi
+}
+
+# Function to start cAdvisor container
+start_cadvisor() {
+  docker start cadvisor
+}
+
+# Function to install cAdvisor
+install_cadvisor() {
+  # Install prerequisites (e.g., curl) if needed
+  # For example, on Debian/Ubuntu:
+  # apt-get update
+  # apt-get install -y curl
+
+  # Install cAdvisor using the provided installation method
+  # For example, for cAdvisor on Docker:
+  docker run -d --name=cadvisor \
+  --privileged \
+  --device=/dev/kmsg \
+  -p 9101:8080 \
+  -v /:/rootfs:ro \
+  -v /var/run:/var/run:ro \
+  -v /sys:/sys:ro \
+  -v /var/lib/docker/:/var/lib/docker:ro \
+  -v /dev/disk/:/dev/disk:ro \
+  gcr.dockerproxy.com/cadvisor/cadvisor:v0.47.1 \
+  --housekeeping_interval=30s \
+  --docker_only=true \
+  --disable_metrics=advtcp,cpu_topology,cpuset,hugetlb,memory_numa,process,referenced_memory,resctrl,sched,tcp,udp,percpu,perf_event,disk,diskIO
+}
+
+# Main script logic
+if [[ $(check_docker;echo $?) -ne 0 ]]; then
+  echo "Docker is not installed or not running. Skipping cAdvisor installation."
+  exit 0
+fi
+
+if [[ $(check_cadvisor;echo $?) -eq 0 ]];then
+  if [[ $(check_cadvisor_running; echo $?) -ne 0 ]];then
+    echo "cAdvisor is not running. Start it ..."
+    start_cadvisor
+  fi
+else
+  echo "cAdvisor is not installed. Install it ..."
+  install_cadvisor
+fi 
